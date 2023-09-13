ab0ba5247f2b6610fcdedf442f327a4412f330a6
LocPhanFullstack <phananhloc03102001@gmail.com>
2023-09-13 17:08:53 +0700

    first commit

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -23,13 +23,13 @@ export class FilesController {
 
     @Post('upload')
     @ResponseMessage('Upload single file')
-    @UseInterceptors(FileInterceptor('file'))
+    @UseInterceptors(FileInterceptor('fileUpload'))
     uploadFile(
         @UploadedFile(
             new ParseFilePipeBuilder()
                 .addFileTypeValidator({
                     fileType:
-                        /^(jpg|jpeg|image\/jpeg|png|image\/png|gif|txt|pdf||application\/pdf|doc|docx|text\/plain)$/i,
+                        /^(jpg|jpeg|image\/jpeg|png|image\/png|gif|txt|pdf|application\/pdf|doc|docx|text\/plain)$/i,
                 })
                 .addMaxSizeValidator({ maxSize: 1024 * 1024 })
                 .build({ errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY }),
---
+++ src/resumes/resumes.controller.ts
@@ -0,0 +1,49 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { ResumesService } from './resumes.service';
+import { CreateUserCvDto } from './dto/create-resume.dto';
+import { UpdateResumeDto } from './dto/update-resume.dto';
+import { ResponseMessage, UserDecorator } from '@/decorator/customize';
+import { IUser } from '@/users/user.interface';
+
+@Controller('resumes')
+export class ResumesController {
+    constructor(private readonly resumesService: ResumesService) {}
+
+    @Post()
+    @ResponseMessage('Create a new resume')
+    create(@Body() createUserCvDto: CreateUserCvDto, @UserDecorator() user: IUser) {
+        return this.resumesService.create(createUserCvDto, user);
+    }
+
+    @Get()
+    @ResponseMessage('Fetch all resumes with paginate')
+    findAll(
+        @Query('current') currentPage: string,
+        @Query('pageSize') limit: string,
+        @Query() qs: string,
+    ) {
+        return this.resumesService.findAll(+currentPage, +limit, qs);
+    }
+
+    @Get(':id')
+    @ResponseMessage('Fetch a resume by id')
+    findOne(@Param('id') id: string) {
+        return this.resumesService.findOne(id);
+    }
+
+    @Patch(':id')
+    @ResponseMessage('Fetch a resume by id')
+    updateStatus(
+        @Param('id') id: string,
+        @Body('status') status: string,
+        @UserDecorator() user: IUser,
+    ) {
+        return this.resumesService.update(id, status, user);
+    }
+
+    @Delete(':id')
+    @ResponseMessage('Delete a resume')
+    remove(@Param('id') id: string, @UserDecorator() user: IUser) {
+        return this.resumesService.remove(id, user);
+    }
+}
